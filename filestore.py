33c1812760e291522ca8f3367d90b364e3a1df9b
Yuta <yatokato@gmail.com>
2014-12-10 23:13:12 -0500

    Integrating dropbox implementation.

--- filestore.py
+++ filestore.py
@@ -1,101 +1,36 @@
 import os
-
-filestoreDirectory = "filestore/"
-
-def writeFileToDisk(encryptedFile): 
-    filename = filestoreDirectory + encryptedFile.encryptedName
-    f = open(filename, 'w+')
-    f.write(encryptedFile.encryptedContents)
-    
-def fileExists(encryptedFilename):
-    return os.path.isfile(filestoreDirectory + encryptedFilename)
-
-# assumes that file exists
-# gets file with only read permission
-# returns an instance of EncryptedFile class
-def getFileFromDisk(encryptedFilename):
-    f = open(filestoreDirectory + encryptedFilename, 'r') 
-    encryptedContents = f.read()
-    f.close()
-    return EncryptedFile(encryptedFilename, encryptedContents)
-
-# assumes that the file exists 
-def deleteFileOnDisk(encryptedFilename):
-    os.remove(filestoreDirectory + encryptedFilename)
-
-# assumes that the file to rename exists
-def renameFileOnDisk(oldName, newName): 
-    os.rename(filestoreDirectory + oldName, filestoreDirectory + newName)
+import DropboxClient
 
 class Filestore():
     def __init__(self):
-        self.fileMap = {} # Convert this to a cache later
+        self.dropboxClient = DropboxClient.DropboxClient()
 
     # Does nothing if the file already exists 
     def addFile(self, encryptedFile):
-        encryptedFilename = encryptedFile.encryptedName 
-        if not fileExists(encryptedFilename):
-            writeFileToDisk(encryptedFile)
+        self.writeFileToDropbox(encryptedFile)
 
-    # Returns None if file does not exist
+    # Returns None if file does not exist, return EncryptedFilename
     def getFile(self, encryptedFilename): 
-        if not fileExists(encryptedFilename):
-            return None 
-        return getFileFromDisk(encryptedFilename)
+        encryptedFilename, encryptedContents = self.dropboxClient.download(encryptedFilename)
+        return EncryptedFile(encryptedFilename, encryptedContents)
 
     # Does nothing if file does not exist 
     def deleteFile(self, encryptedFilename):
-        if not fileExists(encryptedFilename):
-            return None
-        deleteFileOnDisk(encryptedFilename)
+        self.dropboxClient.delete(encryptedFilename)
 
     # Does nothing if the file doesn't already exist
     def updateFile(self, encryptedFilename, encryptedFile): 
-        if not fileExists(encryptedFilename):
-            return None
-        self.deleteFile(encryptedFilename)
-        self.addFile(encryptedFile)
+        self.writeFileToDropbox(encryptedFile)
 
     # Does nothing if the file to rename does not exist 
     # or if the new file name is already used 
     def renameFile(self, encryptedFilename, newEncryptedFilename):
-        if not fileExists(encryptedFilename) or fileExists(newEncryptedFilename):
-            return
-        renameFile(encryptedFilename, newEncryptedFilename)
+        pass 
 
-    """ OLD IN MEMORY CODE 
-    def addFile(self, encryptedFile):
-        encryptedFilename = encryptedFile.encryptedName
-        if encryptedFilename in self.fileMap:
-            raise Exception("File with that name already exists")
-        if encryptedFilename not in self.fileMap:
-            self.fileMap[encryptedFilename] = encryptedFile
-
-    def getFile(self, encryptedFilename):
-        if encryptedFilename not in self.fileMap:
-            return None
-        return self.fileMap[encryptedFilename]
-
-    def deleteFile(self, encryptedFilename):
-        if encryptedFilename in self.fileMap:
-            del self.fileMap[encryptedFilename]
-
-    def updateFile(self, encryptedFilename, encryptedFile):
-        if encryptedFilename in self.fileMap:
-            self.fileMap[encryptedFilename] = encryptedFile
-
-
-
-    def renameFile(self, encryptedFilename, newEncryptedFilename):
-        if encryptedFilename not in self.fileMap:
-            raise Exception("File to rename does not exist")
-        if newEncryptedFilename in self.fileMap:
-            raise Exception("New filename already exists")
-        fileObj = self.fileMap[encryptedFilename]
-        self.fileMap[newEncryptedFilename] = fileObj
-    """
-    
-
+    def writeFileToDropbox(self, encryptedFile): 
+        filename = encryptedFile.encryptedName
+        contents = encryptedFile.encryptedContents
+        self.dropboxClient.upload(filename, contents)
 
 class EncryptedFile():
     def __init__ (self, name, contents):
